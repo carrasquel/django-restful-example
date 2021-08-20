2895bcbd71d360a2a1a15c0d353576aff1375419
Nelson Carrasquel <carrsquel@outlook.com>
2021-08-19 23:14:07 -0400

    Model for ToDo

--- tasksproject/tasks/models.py
+++ tasksproject/tasks/models.py
@@ -1,3 +1,15 @@
+from django.conf import settings
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class ToDo(models.Model):
+
+    task = models.CharField(max_length=80, default='')
+    finished = models.BooleanField(default=False)
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True)
+
+    def __str__(self):
+
+        return _(self.task) or _('Tasks: %s') % ('finished' if self.finished else 'unfinished')
